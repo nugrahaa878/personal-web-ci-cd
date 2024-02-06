23466d79d6361ee92e98ff6a7b106d786e833e2c
arinugraha11 <ari.nugraha@tokopedia.com>
2024-02-06 16:23:26 +0700

    feat: add basename

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/personal-web-ci-cd">
       <MainRoutes />
     </BrowserRouter>
   </React.StrictMode>
