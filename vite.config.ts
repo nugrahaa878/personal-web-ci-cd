83770c56e63f106f28c4e47745f0b670b92b9997
arinugraha11 <ari.nugraha@tokopedia.com>
2024-02-12 16:24:31 +0700

    feat: try to push gcr

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/personal-web-ci-cd/",
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
 });
