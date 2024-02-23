69a0110eaefcc237c88b0fd780dde0604e0896e7
nugrahaa878 <nugrahaa878@gmail.com>
2024-02-23 15:15:47 +0700

    feat: change basename in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/personal-web-ci-cd/",
+  base: "/",
   server: {
     watch: {
       usePolling: true,
