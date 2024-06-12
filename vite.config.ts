09ecd531f690c4c9f03652f8490957481a915f75
nugrahaa878 <nugrahaa878@gmail.com>
2024-06-12 20:03:07 +0700

    feat: fix react-refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactRefresh from "eslint-plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [mode === "development" && reactRefresh, react()].filter(Boolean),
+    plugins: [mode === "development" && react()],
     base: "/",
     server: {
       watch: {
