d2bcdbcd6b5eb51c7f61cb12b8ac1bc14e272715
nugrahaa878 <nugrahaa878@gmail.com>
2024-06-12 19:50:56 +0700

    feat: fix react-refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [mode === "development" && reactRefresh, react()].filter(Boolean),
+    base: "/",
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: 5173, // you can replace this port with any port
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
-  },
+  };
 });
