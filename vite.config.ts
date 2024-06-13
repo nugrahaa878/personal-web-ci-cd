cb91041254dccdbb9e43f387cbd54199e5104499
nugrahaa878 <nugrahaa878@gmail.com>
2024-06-13 16:00:13 +0700

    feat: ssr no external

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    ssr: {
+      noExternal: "true",
+    },
     plugins: [],
     base: "/",
     server: {
