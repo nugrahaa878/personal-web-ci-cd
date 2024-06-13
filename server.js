5300deb90aab4eb2f507b193df7847c840ed7d3b
nugrahaa878 <nugrahaa878@gmail.com>
2024-06-13 14:11:44 +0700

    feat: fix ssr

---
+++ server.js
@@ -0,0 +1,73 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
+import { createServer as createViteServer } from "vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+async function createServer() {
+  console.log("999 LINE 10");
+
+  const app = express();
+
+  console.log("999 LINE 14");
+
+  // Create Vite server in middleware mode and configure the app type as
+  // 'custom', disabling Vite's own HTML serving logic so parent server
+  // can take control
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+  });
+
+  console.log("999 LINE 24");
+
+  // Use vite's connect instance as middleware. If you use your own
+  // express router (express.Router()), you should use router.use
+  // When the server restarts (for example after the user modifies
+  // vite.config.js), `vite.middlewares` is still going to be the same
+  // reference (with a new internal stack of Vite and plugin-injected
+  // middlewares). The following is valid even after restarts.
+  app.use(vite.middlewares);
+
+  console.log("999 LINE 34");
+
+  app.use("*", async (req, res, next) => {
+    console.log("999 LINE 37");
+
+    const url = req.originalUrl;
+
+    console.log("999 LINE 31");
+
+    try {
+      // 1. Read index.html
+      let template = fs.readFileSync(
+        path.resolve(__dirname, "index.html"),
+        "utf-8"
+      );
+
+      template = await vite.transformIndexHtml(url, template);
+
+      const { render } = await vite.ssrLoadModule("/src/entry-server.tsx");
+
+      console.log("999 LINE 422", url);
+
+      const appHtml = await render(url);
+
+      console.log("999 after render", { appHtml });
+
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+
+      // 6. Send the rendered HTML back.
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
+    } catch (e) {
+      vite.ssrFixStacktrace(e);
+      next(e);
+    }
+  });
+
+  app.listen(5173);
+}
+
+createServer();
